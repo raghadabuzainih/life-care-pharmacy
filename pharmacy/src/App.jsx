bac9230fb999fdc4a654819aa10d5104a2d88221
Raghad Fawwaz <raghad.abuzainih@gmail.com>
2025-08-14 01:21:17 +0300

    store-in-cookie

--- pharmacy/src/App.jsx
+++ pharmacy/src/App.jsx
@@ -2,14 +2,17 @@ import './css/App.css'
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from './routes/AppRoutes'
+import { CookiesProvider } from 'react-cookie'
 
 const root = createRoot(document.getElementById('root'))
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <CookiesProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </CookiesProvider>
   )
 }
 
--- pharmacy/src/pages/Login.jsx
+++ pharmacy/src/pages/Login.jsx
@@ -1,9 +1,13 @@
 import users from '../data/users.json'
 import '../css/Login.css'
 import { useLocation, useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import { useCookies } from 'react-cookie'
 
 export const Login = ({isLoggin}) => {
+    const [cookies, setCookie, removeCookie] = useCookies(["user"])
+    const [correctEmail, setCorrectEmail] = useState(true)
+    const [correctPassword, setCorrectPassword] = useState(true)
     const location = useLocation()
     const navigate = useNavigate()
     const from = location.state?.from?.pathname || "/"
@@ -13,21 +17,28 @@ export const Login = ({isLoggin}) => {
         const formData = new FormData(event.currentTarget)
         const email = formData.get('email')
         const password = formData.get('password')
-        //check if user exists(authentication) & password is not empty
-        if(users.find(user => user.email == email) && password){
+        //check if user exists(authentication)
+        if(users.find(user => user.email == email && user.password == password)){
             isLoggin(true)
+            setCorrectEmail(true)
+            setCorrectPassword(true)
+            //store data for 10 minutes
+            setCookie("user", JSON.stringify({email, password}), {path: "/", maxAge: 10*60})
             navigate(from, { replace: true })
+        }else if(users.find(user => user.email == email)){
+            setCorrectEmail(true)
+            setCorrectPassword(false)
+        }else if(!users.find(user => user.email == email)){
+            setCorrectEmail(false)
         }
     }
 
     useEffect(()=>{
-        const timer = setTimeout(()=> {
+        if(!cookies.user){ //logout
             isLoggin(false)
             navigate('/login')
-        }, 10*60*1000) //after 10 minutes
-
-        return () => clearTimeout(timer)
-    }, [isLoggin, navigate])
+        }
+    }, [cookies, navigate, isLoggin])
 
     return(
         <form className="login" onSubmit={handleSubmit}>
@@ -35,10 +46,12 @@ export const Login = ({isLoggin}) => {
             <div className="info">
                     <label htmlFor="email">Email</label>
                     <input type="email" id="email" placeholder="enter your email" name="email" required />
+                    <p style={{display: correctEmail ? 'none' : 'block'}}>incorrect email</p>
                 </div>
                 <div className="info">
                     <label htmlFor="password">Password</label>
                     <input type="password" id="password" placeholder="enter your password" name="password" required />
+                    <p style={{display: correctPassword ? 'none' : 'block'}}>incorrect password</p>
                 </div>
                 <button type="submit">Log in</button>
         </form>
